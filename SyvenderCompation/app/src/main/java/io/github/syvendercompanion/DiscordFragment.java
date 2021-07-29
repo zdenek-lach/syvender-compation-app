5b03092d8ddad1afef37a51aebd049b866bd7e93
Zdeněk Lach <lach.zdenek@gmail.com>
2021-07-30 00:00:29 +0200

    Discord Invite + Reporting bugs
    - Both working flawlessly
    - Email has predefined subject
    https://stackoverflow.com/questions/3470042/intent-uri-to-launch-gmail-app
    modified version of answer from CopsOnRoad
    
    - Discord opens a link as standard

--- SyvenderCompation/app/src/main/java/io/github/syvendercompanion/DiscordFragment.java
+++ SyvenderCompation/app/src/main/java/io/github/syvendercompanion/DiscordFragment.java
@@ -1,5 +1,7 @@
 package io.github.syvendercompanion;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,5 +18,10 @@ public class DiscordFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull @org.jetbrains.annotations.NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_discord,container,false);
+    }
+
+    private void OpenDiscordInvite(){
+        String link = getString(R.string.invite_link);
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(link)));
     }
 }
--- SyvenderCompation/app/src/main/java/io/github/syvendercompanion/MainActivity.java
+++ SyvenderCompation/app/src/main/java/io/github/syvendercompanion/MainActivity.java
@@ -7,9 +7,12 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
@@ -75,17 +78,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             case R.id.nav_music_toggle:
                 switch (isMusicPlaying) {
                     case 1:
-                        isMusicPlaying=DJ.pauseMusic();
+                        isMusicPlaying = DJ.pauseMusic();
                         Toast.makeText(this, R.string.music_play_toast, Toast.LENGTH_LONG).show();
                         break;
 
                     case 0:
-                        isMusicPlaying=DJ.startMusic();
+                        isMusicPlaying = DJ.startMusic();
                         Toast.makeText(this, R.string.music_pause_toast, Toast.LENGTH_LONG).show();
                         break;
                 }
             case R.id.nav_contact_developers:
-                //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new DonateFragment()).commit();
+                sendEmail();
                 break;
             default:
                 Toast.makeText(this, "Weird selection happened", Toast.LENGTH_LONG).show();
@@ -102,5 +105,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         } else {
             super.onBackPressed();
         }
+    }
+
+
+    public void OpenDiscordInvite(View view) {
+        String link = getString(R.string.invite_link);
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(link)));
+    }
+
+
+    public void sendEmail(){
+        Intent emailIntent = new Intent (Intent.ACTION_SEND);
+        emailIntent.setType("message/rfc822");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"devs@syvender.io"});
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Reporting a bug!");
+        startActivity(emailIntent);
     }
 }
