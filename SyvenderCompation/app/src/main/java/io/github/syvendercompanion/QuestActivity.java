b77f8474a78bf58ee46cbf78df790df6baccc43e
Zdeněk Lach <lach.zdenek@gmail.com>
2021-08-08 13:35:38 +0200

    Quest Tracker semi-completed
    - Ticking works (need alignment)
    - Toast works with selection
    TODO: 
    - Save to file
    - Load from file

--- SyvenderCompation/app/src/main/java/io/github/syvendercompanion/QuestActivity.java
+++ SyvenderCompation/app/src/main/java/io/github/syvendercompanion/QuestActivity.java
@@ -1,30 +1,109 @@
 package io.github.syvendercompanion;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class QuestActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    private ArrayList<QuestModelClass> quests = new ArrayList<>();
+    private MultiAdapter adapter;
+    Button btn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        if (getSupportActionBar() != null){
+        if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setDisplayUseLogoEnabled(true);
         }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quest);
+
+        recyclerView = findViewById(R.id.quests_recyclerview_id);//potential problem here?
+        btn = findViewById(R.id.quest_recyclerview_button);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        adapter = new MultiAdapter(this, quests);
+        recyclerView.setAdapter(adapter);
+        CreateListOfData();
+//        CreateListOfMockData();
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (adapter.getSelected().size() > 0) {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    for (int i = 0; i < adapter.getSelected().size(); i++) {
+                        stringBuilder.append(adapter.getSelected().get(i).getName());
+                        stringBuilder.append("\n");
+                    }
+                    ShowToast(stringBuilder.toString().trim());
+
+                } else
+                    ShowToast("No Selection");
+
+            }
+        });
+
     }
 
+    private void ShowToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void CreateListOfMockData() {
+        quests = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            QuestModelClass quest = new QuestModelClass();
+            quest.setName("Quests " + (i + 1));
+            if (i==0){
+                quest.setChecked(true);
+            }
+
+            quests.add(quest);
+        }
+        adapter.setQuests(quests);
+    }
+
+    private void CreateListOfData() {
+        quests = new ArrayList<>();
+//        StringBuilder questBuilder = new StringBuilder(getResources().getStringArray(R.array.questListArray));
+        String[] questList = getResources().getStringArray(R.array.questListArray);
+        for (String value : questList
+        ) {
+            value.trim();
+            System.out.println("QUEST LIST VALUE: " + "'" + value + "'");
+        }
+        for (int i = 0; i < 10; i++) {
+            QuestModelClass quest = new QuestModelClass();
+            quest.setName(questList[i]);
+//            if (i == 0) {
+//                quest.setChecked(true);
+//            }
+
+            quests.add(quest);
+        }
+        adapter.setQuests(quests);
+    }
 
 
     @Override
@@ -40,7 +119,42 @@ public class QuestActivity extends AppCompatActivity {
 
     public class MultiAdapter extends RecyclerView.Adapter<MultiAdapter.MultiViewHolder> {
 
-        public class MultiViewHolder extends RecyclerView.ViewHolder{
+        private Context context;
+        private ArrayList<QuestModelClass> quests;
+
+        public MultiAdapter(Context context, ArrayList<QuestModelClass> quests) {
+            this.context = context;
+            this.quests = quests;
+        }
+
+        public void setContext(Context context) {
+            this.context = context;
+        }
+
+        public void setQuests(ArrayList<QuestModelClass> quests) {
+            this.quests = new ArrayList<>();
+            this.quests = quests;
+            notifyDataSetChanged();
+        }
+
+        @NonNull
+        @Override
+        public MultiViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(context).inflate(R.layout.quest_model, parent, false);
+            return new MultiViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MultiViewHolder holder, int position) {
+            holder.bind(quests.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return quests.size();
+        }
+
+        public class MultiViewHolder extends RecyclerView.ViewHolder {
             private TextView textView;
             private ImageView imageView;
 
@@ -51,7 +165,7 @@ public class QuestActivity extends AppCompatActivity {
 
             }
 
-            void bind(final QuestModelClass questModelClass){
+            void bind(final QuestModelClass questModelClass) {
                 imageView.setVisibility(questModelClass.isChecked ? View.VISIBLE : View.GONE);
                 textView.setText(questModelClass.getName());
                 itemView.setOnClickListener(new View.OnClickListener() {
@@ -65,8 +179,22 @@ public class QuestActivity extends AppCompatActivity {
             }
 
         }
-    }
 
+        public ArrayList<QuestModelClass> getAll() {
+            return quests;
+        }
+
+        public ArrayList<QuestModelClass> getSelected() {
+            ArrayList<QuestModelClass> selected = new ArrayList<>();
+            for (int i = 0; i < quests.size(); i++) {
+                if (quests.get(i).isChecked()) {
+                    selected.add(quests.get(i));
+                }
+            }
+            return selected;
+        }
+
+    }
 
 
     public class QuestModelClass implements Serializable {
