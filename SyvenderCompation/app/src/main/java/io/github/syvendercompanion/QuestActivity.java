68e8ff6127ffb4b7cb4ac34c5c234e5dcf80df2c
Zdeněk Lach <lach.zdenek@gmail.com>
2021-08-08 17:17:05 +0200

    I forgot

--- SyvenderCompation/app/src/main/java/io/github/syvendercompanion/QuestActivity.java
+++ SyvenderCompation/app/src/main/java/io/github/syvendercompanion/QuestActivity.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,6 +18,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.Serializable;
 import java.util.ArrayList;
 
@@ -25,7 +35,8 @@ public class QuestActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     private ArrayList<QuestModelClass> quests = new ArrayList<>();
     private MultiAdapter adapter;
-    Button btn;
+    Button saveButton;
+    Button loadButton;
 
 
     @Override
@@ -39,7 +50,8 @@ public class QuestActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quest);
 
         recyclerView = findViewById(R.id.quests_recyclerview_id);//potential problem here?
-        btn = findViewById(R.id.quest_recyclerview_save_button);
+        saveButton = findViewById(R.id.quest_recyclerview_save_button);
+        loadButton = findViewById(R.id.quest_recyclerview_load_button);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
         adapter = new MultiAdapter(this, quests);
@@ -47,21 +59,38 @@ public class QuestActivity extends AppCompatActivity {
         CreateListOfData();
 //        CreateListOfMockData();
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (adapter.getSelected().size() > 0) {
-                    StringBuilder stringBuilder = new StringBuilder();
-                    for (int i = 0; i < adapter.getSelected().size(); i++) {
-                        stringBuilder.append(adapter.getSelected().get(i).getName());
-                        stringBuilder.append("\n");
-                    }
-                    ShowToast(stringBuilder.toString().trim());
+//        saveButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (adapter.getSelected().size() > 0) {
+//                    StringBuilder stringBuilder = new StringBuilder();
+//                    for (int i = 0; i < adapter.getSelected().size(); i++) {
+//                        stringBuilder.append(adapter.getSelected().get(i).getName());
+//                        stringBuilder.append("\n");
+//                    }
+//                    ShowToast(stringBuilder.toString().trim());
+//
+//                } else
+//                    ShowToast("No Selection");
+//
+//            }
+//        });
 
-                } else
-                    ShowToast("No Selection");
+        saveButton.setOnClickListener(v -> {
+            ArrayList<QuestModelClass> adapterAll = adapter.getAll();
+            Context context = QuestActivity.this.recyclerView.getContext();
+            writeToFile(ConvertQuestToStringForFile(adapterAll), context);
+        });
 
+        loadButton.setOnClickListener(v -> {
+            Context context = QuestActivity.this.recyclerView.getContext();
+            ArrayList<QuestModelClass> loaded = ConvertStringFromFileToQuest(readFromFile(context));
+
+            for (int i = 0; i < loaded.size(); i++) {
+                System.out.println("Load button has loaded this: " + loaded.get(i));
             }
+            FillListOfDataFromFile(loaded);
+
         });
 
     }
@@ -75,7 +104,7 @@ public class QuestActivity extends AppCompatActivity {
         for (int i = 0; i < 20; i++) {
             QuestModelClass quest = new QuestModelClass();
             quest.setName("Quests " + (i + 1));
-            if (i==0){
+            if (i == 0) {
                 quest.setChecked(true);
             }
 
@@ -84,24 +113,106 @@ public class QuestActivity extends AppCompatActivity {
         adapter.setQuests(quests);
     }
 
+    private void writeToFile(String data, Context context) {
+        try {
+            data = chop(data);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("QuestSave.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            System.out.println("WRITE TO FILE IS SAVING THIS: " + data);
+            outputStreamWriter.close();
+            ShowToast("Saving successful");
+        } catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+    private String chop(String data) {
+        return data.substring(0, data.length() - 1);
+    }
+
+
+    private String readFromFile(Context context) {
+        String ret = "";
+
+        try {
+            InputStream inputStream = context.openFileInput("QuestSave.txt");
+
+            if (inputStream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ((receiveString = bufferedReader.readLine()) != null) {
+                    stringBuilder.append("\n").append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        } catch (FileNotFoundException e) {
+            Log.e("quest activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("quest activity", "Can not read file: " + e.toString());
+        }
+        ShowToast("Loading successful");
+        return ret;
+    }
+
+
+    private String ConvertQuestToStringForFile(ArrayList<QuestModelClass> checkedQuestList) {
+        StringBuilder ret = new StringBuilder();
+
+        for (int i = 0; i < checkedQuestList.size(); i++) {
+            ret.append(checkedQuestList.get(i).getName());
+            ret.append("~");
+            ret.append(checkedQuestList.get(i).isChecked());
+            ret.append(",");
+            System.out.println("ConvertQuestToStringForFile has this data:"+ret.toString());
+        }
+        return ret.toString();
+
+    }
+
+    private ArrayList<QuestModelClass> ConvertStringFromFileToQuest(String listOfLoadedQuests) {
+        ArrayList<QuestModelClass> ret = new ArrayList<>();
+        listOfLoadedQuests = listOfLoadedQuests.trim();
+
+        String[] allQuests = listOfLoadedQuests.split(",");
+
+        String[] selectedQuest;
+        for (String quest : allQuests) {
+            System.out.println("Check log for foreach in method 'ConvertStringFromFileToQuest'. Quest value is: " + quest);
+            selectedQuest = quest.split("~");
+            ret.add(new QuestModelClass(selectedQuest[0], selectedQuest[1]));
+        }
+
+        return ret;
+    }
+
+
     private void CreateListOfData() {
         quests = new ArrayList<>();
-//        StringBuilder questBuilder = new StringBuilder(getResources().getStringArray(R.array.questListArray));
         String[] questList = getResources().getStringArray(R.array.questListArray);
         for (String value : questList
         ) {
-            value.trim();
+            value = value.trim();
             System.out.println("QUEST LIST VALUE: " + "'" + value + "'");
         }
-        for (int i = 0; i < questList.length; i++) {
+        for (String s : questList) {
             QuestModelClass quest = new QuestModelClass();
-            quest.setName(questList[i]);
-//            if (i == 0) {
-//                quest.setChecked(true);
-//            }
+            quest.setName(s);
 
             quests.add(quest);
         }
+        adapter.setQuests(quests);
+    }
+
+    private void FillListOfDataFromFile(ArrayList<QuestModelClass> questList) {
+        quests = new ArrayList<>();
+
+        quests.addAll(questList);
+
         adapter.setQuests(quests);
     }
 
@@ -201,6 +312,17 @@ public class QuestActivity extends AppCompatActivity {
         private boolean isChecked = false;
         private String name;
 
+        public QuestModelClass(String name, String isCheckedAsS) {
+            this.name = name;
+            this.isChecked = Boolean.parseBoolean(isCheckedAsS.trim());
+//            System.out.println("QuestModelClass received: '" + isCheckedAsS + "'");
+//            System.out.println("QuestModelClass saved: " + this.isChecked);
+        }
+
+        public QuestModelClass() {
+
+        }
+
         public String getName() {
             return name;
         }
@@ -215,6 +337,11 @@ public class QuestActivity extends AppCompatActivity {
 
         public void setChecked(boolean checked) {
             isChecked = checked;
+        }
+
+        @Override
+        public String toString() {
+            return getName() + "|" + isChecked;
         }
     }
 }
